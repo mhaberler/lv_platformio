fbcf8d7205fac52f507f2cdaad61320c3e8b7ef5
Dennis B <dennis@bemmann.de>
2023-01-13 06:29:51 +0700

    Remove dead code

--- src/main.c
+++ src/main.c
@@ -1,14 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
 #include "lvgl.h"
 #include "app_hal.h"
 
@@ -17,10 +6,7 @@
 int main(void)
 {
 	lv_init();
-
 	hal_setup();
-
   lv_demo_widgets();
-
 	hal_loop();
 }
