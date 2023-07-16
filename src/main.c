d589124799fdb645ce3b5fc9c173c0cc196f375c
Michael Haberler <git@mah.priv.at>
2023-07-16 21:54:24 +0200

    custom works

--- src/main.c
+++ src/main.c
@@ -11,12 +11,26 @@
 #include "app_hal.h"
 #include "lvgl.h"
 
+#ifdef INCLUDE_UI_H
+// #include "../../flowsensor/src/ui/ui.h"
+#include "ui.h"
+#endif
+
 #include "demos/lv_demos.h"
+
+// #ifdef INIT_FUNCTION
+// void INIT_FUNCTION(void);
+// #endif
 
 int main(void) {
   lv_init();
 
   hal_setup();
+
+#ifdef INIT_FUNCTION
+  INIT_FUNCTION();
+#endif
+
 #if LV_USE_DEMO_WIDGETS
   lv_demo_widgets();
 #endif
