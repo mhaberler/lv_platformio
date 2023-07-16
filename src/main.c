c3a1991d68d0fc9fd7eb53c61661244409597989
Michael Haberler <git@mah.priv.at>
2023-07-16 20:48:33 +0200

    wip

--- src/main.c
+++ src/main.c
@@ -1,26 +1,29 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
 
-
-#include "lvgl.h"
 #include "app_hal.h"
+#include "lvgl.h"
 
 #include "demos/lv_demos.h"
 
-int main(void)
-{
-	lv_init();
+int main(void) {
+  lv_init();
 
-	hal_setup();
-
+  hal_setup();
+#if LV_USE_DEMO_WIDGETS
   lv_demo_widgets();
+#endif
 
-	hal_loop();
+#if LV_USE_DEMO_KEYPAD_AND_ENCODER
+  lv_demo_keypad_encoder();
+#endif
+
+  hal_loop();
 }
